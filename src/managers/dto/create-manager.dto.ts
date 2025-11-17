b637d7ea96fc38209689a3a373a945849aeacf59
kusum0711 <meghrajanikusum@gmail.com>
2025-11-17 18:42:46 +0100

    Manager Management done sucessfully

---
+++ src/managers/dto/create-manager.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Matches, MinLength } from 'class-validator';
+
+export class CreateManagerDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail({}, {message: 'Invalid email'})
+  email: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  team_id: number;
+  
+
+}
